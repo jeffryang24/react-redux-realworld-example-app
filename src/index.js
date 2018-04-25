fd2a0cac6c6b3df6224aaaa1c3cccfb74f98ce4f
Jeffry Angtoni <jeffryangtoni24@gmail.com>
2018-04-25 14:52:15 +0700

    -> implement router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import store from './store';
 
 import App from './components/App';
+import Login from './components/Login';
 
 import ReactDOM from 'react-dom';
 import React from 'react';
 import { Provider } from 'react-redux';
-import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 
 // root Element
 const rootElement = document.getElementById('root');
@@ -14,7 +15,10 @@ const rootElement = document.getElementById('root');
 ReactDOM.render((
   <Provider store={store}>
     <Router>
-      <Route exact path="/" component={App} />
+      <div>
+        <Route exact path="/" component={App} />
+        <Route path="/login" component={Login} />
+      </div>
     </Router>
   </Provider>
 ), rootElement);
